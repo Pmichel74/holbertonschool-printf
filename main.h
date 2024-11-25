f8f3191ff5184100d5f7b314edb0b8a6364c8cba
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-25 17:06:30 +0100

    fixing incrementation + structures

--- main.h
+++ main.h
@@ -5,10 +5,17 @@
 #include <stdarg.h>
 
 /**
- *
- *
+ * * struct func_type - type structure
+ * @t: pointer to the argument
+ * @f: pointer-function associated with the argument
  */
+typedef struct func_type
+{
+	char *t;
+	int (*f)(va_list);
+} func_t;
 
+int (*get_func(const char *format))(va_list);
 int _putchar(char c);
 int _printf(const char *format, ...);
 void custom_printf(const char *format, ...);
