b3d6dbf659a35631840c7823f1cf9d256c3a0f81
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-25 19:44:20 +0100

    change code structure

--- _printf.c
+++ _printf.c
@@ -1,69 +1,44 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdarg.h>
-
+#include <unistd.h>
+#include "main.h"
 /**
- * _printf - A simple implementation of a formatted output function.
- * @format: A string that specifies how subsequent arguments (after the format)
- * are converted for output. It can contain regular characters to be
- * printed as-is, and format specifiers starting with '%' to format
- * the output. Supported specifiers include:
- * - 'c': for printing a character.
- * - 's': for printing a string.
- * - '%': for printing a literal percent sign.
+_printf - main function
+@format: const char format
  *
- * Return: The total number of characters printed,
- * or a negative value on error.
- *
+Description: function that produces output according to a format.
+Return: Count of the charactere printed.
  */
 int _printf(const char *format, ...)
 {
-int count = 0, i = 0;
-char c;
-char *s;
-
 va_list args;
-va_start(args, format);
-
-while (format[count])
-{
-	if (format[count] == '%')
+int count = 0;
+int i = 0;
+	va_start(args, format);
+	while (*format)
 	{
-
-		if (format[count] == 'c')
-		{
-			c = (char)va_arg(args, int);
-
-			write(1, &c, 1);
-		}
-		else if (format[count] == 's')
-		{
-			s = va_arg(args, char*);
-			while (*s)
-			{
-				write(1, s, 1);
-				s++;
-				i++;
-			}
-		}
-		else if (format[count] == '%')
-		{
-			write(1, "%", 1);
-		}
-		else
-		{
-			write(1, "%", 1);
-			write(1, format, 1);
-			count += 2;
-		}
+	if (*format == '%' && *(format + 1))
+	{
+	format++;
+	if (*format == 'c')
+	{
+	char c = va_arg(args, int);
+	count += write(1, &c, 1);
+	}
+	else if (*format == 's')
+	{
+	char *str = va_arg(args, char *);
+	for (i = 0; str && str[i]; i++)
+	count += write(1, &str[i], 1);
+	if (!str)
+	count += write(1, "(null)", 6);
+	}
+	else if (*format == '%')
+	count += write(1, "%", 1);
 	}
 	else
-	{
-		_putchar(format[count]);
+	count += write(1, format, 1);
+	format++;
 	}
-	count++;
-}
 	va_end(args);
-	count += i;
 	return (count);
-	}
+}
