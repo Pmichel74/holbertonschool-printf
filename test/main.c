6265c1d45427353a29c0be74964b1567a002bf3f
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-26 15:10:09 +0100

    add b to main.c

--- test/main.c
+++ test/main.c
@@ -18,6 +18,7 @@ int main(void)
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
+    _printf("%b\n", 98);
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
@@ -41,14 +42,4 @@ int main(void)
     _printf("Unknown:[%r]\n");
     printf("Unknown:[%r]\n");
     return (0);
-    /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-    _printf("%b\n", 98);
-    return (0);
-}
 }
