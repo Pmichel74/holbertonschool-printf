2e4f4d08e3980d232992d0003447bfec600917e1
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-26 15:55:59 +0100

    merge 2 files in one

--- handler.c
+++ handler.c
@@ -76,3 +76,35 @@ int percent_handler(const char *str, va_list list, int *i)
 
 	return (2);
 }
+/**
+ * _strlen - Calculate the length of a string
+ * @str: String
+ *
+ * Return: Length
+ **/
+int _strlen(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * print - print char.
+ * @str: string.
+ *
+ * Return: string length.
+ */
+
+int print(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+		_putchar(str[i]);
+
+	return (i);
+}
