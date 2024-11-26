da877d6f09169207f59e8eb20607760e9255feda
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-26 09:27:03 +0100

    change header

--- main.h
+++ main.h
@@ -1,18 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <unistd.h>
 #include <stdarg.h>
 
-/**
- * * struct func_type - type structure
- * @t: pointer to the argument
- * @f: pointer-function associated with the argument
- */
-
-/* int _putchar(char c); */
+int _putchar(char c);
 int _printf(const char *format, ...);
-/* int print_integer(va_list);*/
-
-
 #endif
